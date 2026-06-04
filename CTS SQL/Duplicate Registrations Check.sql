52875d2e44e5adec800e58fb353b65d74cd1c368
sharvesh-rp <sit23it032@sairamtap.edu.in>
2026-06-04 15:20:25 +0530

    Add files via upload

---
+++ CTS SQL/Duplicate Registrations Check.sql
@@ -0,0 +1,4 @@
+SELECT user_id,event_id,COUNT(*) AS duplicates
+FROM Registrations
+GROUP BY user_id, event_id
+HAVING COUNT(*) > 1;
---
+++ CTS SQL/Peak Session Hours.sql
@@ -0,0 +1,4 @@
+SELECT event_id, COUNT(*) AS session_count
+FROM Sessions
+WHERE TIME(start_time) BETWEEN '10:00:00' AND '12:00:00'
+GROUP BY event_id;
